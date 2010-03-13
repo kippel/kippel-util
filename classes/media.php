fcd0bf7575f338405c5ed34adaf4e05e00fdefa6
kippel <kippeld@gmail.com>
2010-03-13 15:41:53 +0100

    media min

--- classes/media.php
+++ classes/media.php
@@ -5,24 +5,49 @@ class Media {
 
     public static function render_scripts(Array $javascripts ){
     
-          foreach ($javascripts as $js){
+        if (count($javascripts)) ==0)
+            return FALSE;
+    
+    
+        if (!IN_PRODUCTION){
+    
+            foreach ($javascripts as $js){
               
-              echo HTML::script("media/js/".$js);
+                echo HTML::script("media/js/".$js);
               
-          }
+            }
+            
+        }else{
                                               
-                                              
+            $js = implode(",",$javascripts);
+           
+            ?><script type="text/javascript" src="<?=url::base();?>min/index.php?b=media/js&amp;f=<?=$js?>"></script><?
+                     
+        }
+                                                                         
     
     }
 
     public static function render_styles( Array $styles){
                
-           foreach ($styles as $style){
+           if (count($styles))==0)
+               return FALSE;    
+               
+           if (!IN_PRODUCTION){
+          
+               foreach ($styles as $style){
     
-               echo HTML::style("media/styles/".$style);
+                   echo HTML::style("media/styles/".$style);
     
-           }                                                                    
-    
+               }
+               	                                                                    
+          }else{
+               
+               $styles = implode(",",$styles);
+               ?><link rel='stylesheet' type='text/css' href='<?=url::base();?>min/index.php?b=media/styles&amp;f=<?=$styles?>'></script><?
+                       
+          } 
+          
     }
 
 
